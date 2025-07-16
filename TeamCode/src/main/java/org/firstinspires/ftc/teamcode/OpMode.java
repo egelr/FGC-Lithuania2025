a7c1b27e54d9566e1e170f56912b7b2936094a3f
egelr <egle.rupeikyt@gmail.com>
2025-07-16 13:07:16 +0300

    drivetrain first

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Variables;
+
+@TeleOp(name = "TeleOpMode")
+public class OpMode extends LinearOpMode {
+
+
+    private Motor fL, fR, bL, bR;
+    //Creating drive speed variable
+    public double drive_speed = 1;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //Creating Drivetrain Motors and Setting their behaviour to "brake"
+        fL = new Motor(hardwareMap, "leftFront", 28, 500);
+        fR = new Motor(hardwareMap, "rightFront", 28, 500);
+        bL = new Motor(hardwareMap, "leftBack", 28, 500);
+        bR = new Motor(hardwareMap, "rightBack", 28, 500);
+
+        fL.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        fR.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        bL.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        bR.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        //Creating the Mecanum Drivetrain
+        MecanumDrive drive = new MecanumDrive(fL, fR, bL, bR);
+
+        //Creating timer variables
+        ElapsedTime timer = new ElapsedTime();
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+            //Drivetrain controls
+            drive.driveRobotCentric(
+                    gamepad1.left_stick_x * drive_speed,
+                    -gamepad1.left_stick_y * drive_speed,
+                    gamepad1.right_stick_x * drive_speed,
+                    false
+
+            );
+            //Drivetrain Motors speed Change controls
+            if (gamepad1.right_trigger > 0.5) {
+                drive_speed = 0.45;
+            } else {
+                drive_speed = 1;
+            }
+
+        }
+
+    }
+}
